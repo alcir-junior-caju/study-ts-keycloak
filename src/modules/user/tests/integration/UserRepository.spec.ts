31ad79c63491973a029384e8c050a4f60575df6f
Alcir Junior <junior@cajucomunica.com.br>
2024-03-31 21:43:50 -0300

    test: :white_check_mark: change repository tests

--- src/modules/user/tests/integration/UserRepository.spec.ts
+++ src/modules/user/tests/integration/UserRepository.spec.ts
@@ -1,11 +1,6 @@
 import { type ConnectionInterface, EmailValueObject, IdValueObject, NameValueObject, PgPromiseAdapter, TaxIdValueObject } from '@modules/shared'
 import { UserEntity, UserRepository } from '@modules/user'
 
-const userStub = {
-  name: new NameValueObject('John Doe'),
-  email: new EmailValueObject('johndoe@rmail.com')
-}
-
 describe('UserRepository Integration Tests', () => {
   let connection: ConnectionInterface
   let userRepository: UserRepository
@@ -15,14 +10,16 @@ describe('UserRepository Integration Tests', () => {
     userRepository = new UserRepository(connection)
   })
 
-  afterEach(async () => {
+  afterAll(async () => {
     await connection.query('DELETE FROM keycloak.users')
     await connection.close()
   })
 
   it('should be get a user', async () => {
     const userEntity = new UserEntity({
-      ...userStub,
+      id: new IdValueObject('83e032fc-e3e8-4b28-893d-a315c6a0f977'),
+      name: new NameValueObject('John Doe'),
+      email: new EmailValueObject('johndoe@email.com'),
       taxId: new TaxIdValueObject('34866916001')
     })
     await userRepository.save(userEntity)
@@ -34,12 +31,14 @@ describe('UserRepository Integration Tests', () => {
   })
 
   it('should be throw error when user not found', async () => {
-    await expect(userRepository.find('e9de5d53-1dae-4f28-ac7c-9a762164ba98')).rejects.toThrow('user_not_found')
+    await expect(userRepository.find('cb448d8d-4e08-480a-bf4a-1ed640870aeb')).rejects.toThrow('user_not_found')
   })
 
   it('should be create a user', async () => {
     const userEntity = new UserEntity({
-      ...userStub,
+      id: new IdValueObject('e935b023-a6b3-4a6d-8827-eb5970e502ac'),
+      name: new NameValueObject('Jane Doe'),
+      email: new EmailValueObject('janedoe@email.com'),
       taxId: new TaxIdValueObject('74445721000')
     })
     await userRepository.save(userEntity)
@@ -52,14 +51,16 @@ describe('UserRepository Integration Tests', () => {
 
   it('should be update a user', async () => {
     const userEntity = new UserEntity({
-      ...userStub,
+      id: new IdValueObject('0a0b9da8-f2ca-4317-b72c-14632ce14389'),
+      name: new NameValueObject('Charli Doe'),
+      email: new EmailValueObject('charlidoe@email.com'),
       taxId: new TaxIdValueObject('71428793860')
     })
     await userRepository.save(userEntity)
     const userUpdated = {
       id: new IdValueObject(userEntity.id.value),
-      name: new NameValueObject('Jane Doe'),
-      email: new EmailValueObject('janedoe@email.com'),
+      name: new NameValueObject('Charlie Doe'),
+      email: new EmailValueObject('charliedoe@email.com'),
       taxId: new TaxIdValueObject('87748248800')
     }
     const userUpdatedEntity = new UserEntity(userUpdated)
